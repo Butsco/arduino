2fd0d8c186f2f5e37e738606629fb631f67422e4
Steven Reekmans <steven.reekmans@mobilevikings.com>
2015-04-14 20:57:02 +0200

    change is coming

--- Quiz_0_0_2_buttons/Quiz_0_0_2_buttons.ino
+++ Quiz_0_0_2_buttons/Quiz_0_0_2_buttons.ino
@@ -6,31 +6,15 @@
 // Hardware configuration
 int thisbutton = 1;
 
-const int RedLed = 8;
-const int GreenLed = 7;
-const int BlueLed = 6;
-const int BigButton = 4;
-const int ButtonA = A0;
-const int ButtonB = A1;
-const int ButtonC = A2;
-const int ButtonD = A3;
+const int RGB[3] = {6,8,7};
 
-unsigned long answer = 0;
+const long DEBOUNCE = 50;
+int READING = LOW;
+const int BUTTON[4] = {4, A3, A2, A1};
+int state[4] = {LOW, LOW, LOW, LOW};
+int last[4] = {LOW, LOW, LOW, LOW};
+long debounce[4] = {0, 0, 0, 0};
 
-int buttonStateA = 0; 
-int buttonStateB = 0; 
-int buttonStateC = 0; 
-int buttonStateD = 0; 
-int BigButtonState = 0;
-int BigButtonReading;
-int LastBigButtonState = LOW;
-long LastDebounceTimeBig = 0;
-long DebounceDelay = 40;
-boolean Answered = false;
-boolean Reset = false;
-int others[2];
-boolean other_answers = false;
-int Master_Reset;
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 RF24 radio(9,10);
 // Radio pipe addresses for the 2 nodes to communicate.
@@ -38,15 +22,13 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 void setup(void)
 {
-//define pin modi
-  pinMode(RedLed, OUTPUT);
-  pinMode(GreenLed, OUTPUT);
-  pinMode(BlueLed, OUTPUT);
-  pinMode(BigButton, INPUT);
-  pinMode(ButtonA, INPUT);
-  pinMode(ButtonB, INPUT);
-  pinMode(ButtonC, INPUT);
-  pinMode(ButtonD, INPUT);
+  pinMode(RGB[0], OUTPUT);
+  pinMode(RGB[1], OUTPUT);
+  pinMode(RGB[2], OUTPUT);
+  pinMode(BUTTON[0], INPUT);
+  pinMode(BUTTON[1], INPUT);
+  pinMode(BUTTON[2], INPUT);
+  pinMode(BUTTON[3], INPUT);
   
   Serial.begin(57600);
   printf_begin();
@@ -69,45 +51,79 @@ void setup(void)
 }
 
 void loop(void)
-//games will begin in modus 1, you can choosse an other modus after everyone has pushed his button
 {
-  Push();
-  if (Answered == true)
-   {
+  
+  checkButtons();
+  
+  analogWrite(RGB[0], 255);
+  analogWrite(RGB[1], (state[0] | state[2])*255);
+  analogWrite(RGB[2], (state[0] | state[3])*255);
+
+  return;
+  
+  
+  
+}
+
+void checkButtons()
+{
+  for(int i=0; i<4; i++) {
+    READING = digitalRead(BUTTON[i]);
+    
+    if(READING != last[i]) 
+    {
+        debounce[i] = millis();
+        Serial.println(millis() - debounce[i]);
+    }
+    
+    if(millis() - debounce[i] > DEBOUNCE) 
+    { 
+      
+      if(READING != state[i]) {
+        state[i] = READING;
+        
+        if(state[i] == HIGH) {
+          sendStuff(i);
+        }
+      }
+ 
+      
+    }  
+    
+    last[i] = READING;
+  }
+
+}
+
+
+void sendStuff(char x) {
+/*
      radio.startListening(); // luister eerst gedurende 200ms als niemand anders aan het antwoorden is
      unsigned long started_listening = millis();
+     
      while (millis()-started_listening < 200)
      {
        while (!other_answers)
        {
          other_answers = radio.read(others, sizeof(others));
         }
-     }
-     if (other_answers == true)//als er andere antwoorden zijn
-     {
-     while (!Reset)
-       {
-       digitalWrite(RedLed, HIGH);//laat bezet led branden en wacht op reset
-       Master_Reset = radio.read(others, sizeof(others));
-       if(Master_Reset = 1111)
-         {
-         Reset = true;
-         }
-       }
-     }
-     else // stuur antwoord maar 
-     {
-       radio.stopListening(); // First, stop listening so we can talk. 
-       unsigned long time = millis(); // Take the time, and send it.  This will block until complete
-       printf("Now sending %lu...",answer);
-       radio.write( &answer , sizeof(answer) );
-       radio.startListening(); // Now, continue listening
-       unsigned long started_waiting_at = millis(); // Wait here until we get a response, or timeout (250ms)
-       bool timeout = false;
-       while ( ! radio.available() && ! timeout )
+     }*/
+     
+
+     radio.stopListening(); // First, stop listening so we can talk. 
+     unsigned long time = millis(); // Take the time, and send it.  This will block until complete
+     printf("Now sending...");
+ 
+     radio.write( &x , sizeof(x) );
+     radio.startListening(); // Now, continue listening
+     unsigned long started_waiting_at = millis(); // Wait here until we get a response, or timeout (250ms)
+     bool timeout = false;
+     
+     while ( ! radio.available() && ! timeout )
        if (millis() - started_waiting_at > 100 )
-       timeout = true;
-        // Describe the results
+         timeout = true;
+         
+          // Describe the results
       if ( timeout )
       {
         printf("Failed, response timed out.\n\r");
@@ -121,56 +137,10 @@ void loop(void)
       
       printf("Got response %lu, round-trip delay: %lu\n\r",got_time,started_waiting_at-millis()); // Spew it
     }
-     }
-   } 
+     
+   
+
 }
 
 
-void Push()//debounce if the big button is used
-{
-  BigButtonReading = digitalRead(BigButton);
-  if(BigButtonReading != LastBigButtonState) 
-  {
-    LastDebounceTimeBig = millis();
-  }
-  if(millis() - LastDebounceTimeBig > DebounceDelay) 
-  {
-    if(BigButtonReading != BigButtonState)
-    {
-      BigButtonState = BigButtonReading;
-      if (BigButtonState == HIGH)
-        {  
-        //test with led
-        answer = 00 & thisbutton & 11 & 1;
-        Answered = true;
-        }
-        else
-        Answered = false;
-    }
-  }
-LastBigButtonState = BigButtonReading;
-}
 
-
-void Options()
-{
-   buttonStateA = digitalRead(ButtonA);
-  buttonStateB = digitalRead(ButtonB);
-  buttonStateC = digitalRead(ButtonC);
-  buttonStateD = digitalRead(ButtonD);
-  if (buttonStateA == HIGH ||  buttonStateC == HIGH || buttonStateD == HIGH ) 
-  {
-    digitalWrite(RedLed, HIGH);
-    answer = 1;
-  }
-  else if (buttonStateB == HIGH) 
-  {
-    digitalWrite(BlueLed, HIGH);
-    answer = 2;
-  }
-  else
-  {
-    digitalWrite(RedLed, LOW);
-    digitalWrite(BlueLed, LOW);
-  } 
-}
---
+++ smashbutton/smashbutton.ino
@@ -0,0 +1,28 @@
+const int buttonPin = A0;
+
+int buttonState = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+ 
+  Serial.begin(9600); 
+}
+
+int state = HIGH;
+int button = 0;
+
+void loop(){
+  button = digitalRead(buttonPin);
+  
+  if(button != state) {
+    state = button;
+    if (button == HIGH) {     
+      Serial.println("1"); 
+    } 
+    else {
+      Serial.println("0"); 
+    }
+  }
+  
+  delay(100);
+}
